15501e9a5b1a382b41d4dc8715686256859bae81
Uttiyo <uom90000@gmail.com>
2025-09-26 17:32:46 +0530

    commit

--- mychatbot/vite.config.js
+++ mychatbot/vite.config.js
@@ -2,7 +2,6 @@
 
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite'
 
 // The config must use the plugin
 export default defineConfig({
